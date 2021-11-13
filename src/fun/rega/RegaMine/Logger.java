74b5acac2bfc15f08f8b01404d0fb6a879002703
TheRega <44639867+therega@users.noreply.github.com>
2021-11-13 17:31:59 +0300

    Update Logger.java

--- src/fun/rega/RegaMine/Logger.java
+++ src/fun/rega/RegaMine/Logger.java
@@ -1,21 +1,21 @@
-package fun.rega.WaterFarmer;
+package fun.rega.RegaMine;
 
 import org.bukkit.*;
 
 public class Logger {
 	public static void clear(final String text) {
-        Bukkit.getConsoleSender().sendMessage(Utils.color("§e§l(" + Main.getInstance().getName() + ") " + text));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("Â§eÂ§l(" + Main.getInstance().getName() + ") " + text));
     }
     
     public static void info(final String text) {
-        Bukkit.getConsoleSender().sendMessage(Utils.color("§e§l(" + Main.getInstance().getName() + ") " + text));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("Â§eÂ§l(" + Main.getInstance().getName() + ") " + text));
     }
     
     public static void warn(final String text) {
-        Bukkit.getConsoleSender().sendMessage(Utils.color("§e§l(" + Main.getInstance().getName() + ") " + text));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("Â§eÂ§l(" + Main.getInstance().getName() + ") " + text));
     }
     
     public static void error(final String text) {
-        Bukkit.getConsoleSender().sendMessage(Utils.color("§e§l(" + Main.getInstance().getName() + ") " + text));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("Â§eÂ§l(" + Main.getInstance().getName() + ") " + text));
     }
 }
